87c4c97efc6b0f72daf6dd4a3d2ac31f118ae199
Jack <0319024@schoolsnet.act.edu.au>
2021-05-30 11:12:31 +1000

    enemy now has an alert symbol to show that they have seen the player

---
+++ Assets/Assets/combat_handler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class combat_handler : MonoBehaviour
+{
+    public static bool Melee_combat_handler(Transform attacker, Transform victim, string mask_name)
+    {
+
+        int mask = 1 << LayerMask.NameToLayer(mask_name);
+        mask |= 1 << LayerMask.NameToLayer("Default");
+        RaycastHit2D hit = Physics2D.Raycast(attacker.position, attacker.up, 1.5f, mask);
+        if (victim.tag != null)
+        {
+            if (hit.collider.tag == victim.tag)
+            {
+                //Debug.Log(hit.collider.tag);
+                return true;
+            }
+            else
+            {
+                //Debug.Log(hit.collider.tag);
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+
+        
+    }  
+}
